32550655a2547ee32cfb857b8b2aa21e61ebc486
Ashish Varyani <ashuvaryani123@gmail.com>
2020-07-05 01:40:04 +0530

    adding all the files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+import {Provider} from 'react-redux'
+import store from './redux/store'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+         <CakeContainer/> 
+         </Provider>    
+         </div>
+  );
+}
+
+export default App;
